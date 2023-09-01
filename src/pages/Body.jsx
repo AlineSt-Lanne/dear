46fd73bd850ff24218a80205c236e954e0dfb419
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2023-09-01 11:54:58 +0200

    create page intro + composents

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,6 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Intro from "./Intro";
+
 export default function Body() {
     return (
       <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Intro />} />
+          </Routes>
+        </Router>
       </div>
     );
   }
